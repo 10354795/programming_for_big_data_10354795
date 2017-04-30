6870d139da56bb58f15d4fb22ec202902710dfd2
Thomas O'Brien <10354795@mydbs.ie>
2017-04-30 22:55:08 +0100

    Update programs and added comments to explain how programs work

--- CA4/CA4_simple.py
+++ CA4/CA4_simple.py
@@ -1,42 +1,58 @@
+# Thomas O'Brien ID 10354795
+
+# Program to extract data from a log of support calls and analyse the activites involved.
+
+# Define function to read in file and strip out spaces and trim end of line characters.
+# Save the information into a file called data.
 
 def read_file(changes_file):
-    # use strip to strip out spaces and trim the line.
     data = [line.strip() for line in open(changes_file, 'r')]
     return data
 
+# Define function to extract the required data from the log (text) file
+
 def get_commits(data):
-    sep = 72*'-'
-    commits = []
-#    current_commit = None
-    index = 0
-    while index < len(data):
-        try:
-            # parse each of the commits and put them into a list of commits
-            details = data[index + 1].split('|')
-            # the author with spaces at end removed.
-            commit = {'revision': details[0].strip(),
-                'author': details[1].strip(),
-                'date': details[2].strip(),
-                'number_of_lines': details[3].strip().split(' ')[0],
-                'changes': data[index+2:data.index('',index+1)],
+    sep = 72*'-'          # Create a variable (sep) to use in identifying the separators between support calls (line of 72 hyphens)
+    commits = []          # Set up list to store the extracted data.
+    index = 0             # initialise index counter to zero to start counting lines in the log file
+    while index < len(data):      # Loop through the data until you reach the number of lines in the data
+        try:                      # Use try except to protect failure of program if index errors occur
+#
+            details = data[index + 1].split('|')        # Split the first four elements of the support calls using "|" character to separate the elements and put them into a list called commits
+            commit = {'revision': details[0].strip(),   # strip and save the zero'th element as "revision" in the list 
+                'author': details[1].strip(),           # Strip and save the 2nd element as name as the 
+                'date': details[2].strip(),             # Strip and save the 3rd element as date
+                'number_of_lines': details[3].strip().split(' ')[0],    # Split the 4th element to remove word "line" and strip spaces. This indicates the number of lines in the comment at end of the support cal text 
+                'changes': data[index+2:data.index('',index+1)],        # Split out the changes starting on the second line (index +1) and continue until you reach a blank (empty) line.
+#
+# The following line splits out the comments.
+# It finds the separator (hyphens) line (sep) and then counts back by the number of comments lines ('number_of_lines') in the support call to identfy the comments.
+# It then extracts the comments from that line forward to separator line.
+#
                 'comment' : data[data.index(sep, index + 1) - int(details[3].strip().split(' ')[0]) : data.index(sep, index + 1)]
                 }
-            commits.append(commit)
-            index = data.index(sep, index + 1)
+            commits.append(commit)              # Append all the elements to the list (commits)
+            index = data.index(sep, index + 1)  # increment the index to the next support call (index line) based on the location of the separator line
         except IndexError:
             break
-    return commits
+    return commits         # Returns the list of data in the commits list when it reaches the end of the file.
+
+# The following function counts the number of calls that each author (help desk agents) performs. 
 
 def get_authors(commits):
-    authors = {}
+    authors = {}            # initialise list to identify and count entries for each author.
     for commit in commits:
-        author = commit['author']
-        if author not in authors:
-            authors[author] = 1
-        else:
+        author = commit['author']   # loop through commits list and identify each author 
+        if author in authors:       # if the author already exists in authors list, increment the counter by 1.
             authors[author] = authors[author] + 1
-    return authors
+        else:                       
+            authors[author] = 1     # if the author does not exist in authors list, initialise the count to 1.
 
+    return authors                  # return list of authors and the count of their occurance in the file.
+
+# The following functions counts the number of help desk cals that have 1 comment line at the end of the call
+# It is used for test purposes.
+#
 def get_number_of_1_comment_lines(commits):
     comment_lines_1 = 0
     for commit in commits:
@@ -46,6 +62,9 @@ def get_number_of_1_comment_lines(commits):
             continue
     return comment_lines_1
 
+# The following functions counts the number of help desk cals that have 4 comment line at the end of the call
+# It is used for test purposes
+#
 def get_number_of_4_comment_lines(commits):
     comment_lines_4 = 0
     for commit in commits:
@@ -55,15 +74,23 @@ def get_number_of_4_comment_lines(commits):
             continue
     return comment_lines_4
 
-if __name__ == '__main__':
-    # open the file - and read all of the lines.
-    changes_file = 'changes_python.txt'
-    data = read_file(changes_file)
-    commits = get_commits(data)
+# The following calls the funtions to run the program
 
-    # print the number of lines read
-    print(len(data))
-    #print(commits)
-    print(commits[11])
+if __name__ == '__main__':
+#
+    changes_file = 'changes_python.txt'     # open the file and read all of the lines.
+    data = read_file(changes_file)          # This reads the information into a variable called data in meomory
+    commits = get_commits(data)             # call the funtion to strip the data.
+    
+    print("")
+    print("Below is the total number of lines in the log file and the number of helpdesk calls(commits)")
+    print(len(data))        # print the number of lines read from the log file changes_python.txt
+    print(len(commits))     # print the number of helpd desk calls (commits) in the log file
+#    print("")
+#    print(commits[10])      # does a sample print of the data in index 10 of the commits list
+    print("")
+    print("Below is a list of the help desk agents (authors) and the number of calls that they handle.")
+    print (get_authors(commits))
+
 #    print(commits[2]['author'])
-    print(len(commits))
+#    print(commits)
--- CA4/CA4_simple_test.py
+++ CA4/CA4_simple_test.py
@@ -1,18 +1,33 @@
+# Thomas O'Brien ID 10354795
+
+# Program to test the program that extracts data from a log of support calls.
+
+
 import unittest
+
+# import the funtions from the commits program in CA4_simple.py
 
 from CA4_simple import get_authors, get_commits, get_number_of_1_comment_lines, get_number_of_4_comment_lines, read_file
 
-class TestCommits(unittest.TestCase):
+class TestCommits(unittest.TestCase):       # instantiate the test class and call test cases.
 
+# Read the log file into varaible called self.data in memory
+                 
     def setUp(self):
         self.data = read_file('changes_python.txt')
-
+  
+# Test that the log file has 5255 lines
+      
     def test_number_of_lines(self):
-        self.assertEqual(5255, len(self.data))
+        self.assertEqual(5255, len(self.data))     
+
+# Test that there are 422 service calls (commits) identified in the log file
 
     def test_number_of_commits(self):
         commits = get_commits(self.data)
         self.assertEqual(422, len(commits))
+
+# Test that there are 10 authors in the log file and that Thomas handles 191 calls.
 
     def test_number_of_authors(self):
         commits = get_commits(self.data)
@@ -21,20 +36,24 @@ class TestCommits(unittest.TestCase):
         self.assertEqual(10, len(authors))
         self.assertEqual(191, authors['Thomas'])
 
+# Test that Vincent is the author of index 10 and the revision code is r1551332 
+
     def test_first_commit(self):
         commits = get_commits(self.data)
-        self.assertEqual('Thomas', commits[0]['author'])
-        self.assertEqual('r1551925', commits[0]['revision'])
+        self.assertEqual('Vincent', commits[10]['author'])
+        self.assertEqual('r1551332', commits[10]['revision'])
+
+#Test that the count commits with 1 comment line is 348
 
     def test_count_comments_with_1_line(self):
         commits = get_commits(self.data)
         self.assertEqual(348, get_number_of_1_comment_lines(commits))
-#        print ("*** The count of 1 comment lines is ", get_number_of_1_comment_lines(commits))
- 
+
+#Test that the count of commits with 4 comment lines is 7 
+
     def test_count_comments_with_4_lines(self):
         commits = get_commits(self.data)
         self.assertEqual(7, get_number_of_4_comment_lines(commits))
-#        print ("*** The count of 4 comment lines is ", get_number_of_4_comment_lines(commits))
      
 
 if __name__ == '__main__':
